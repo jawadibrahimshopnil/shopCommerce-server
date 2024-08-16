abee5162b4c47b61be0c284089d9cf602fdfb99d
jawadibrahim <jawadibrahimshopnil01@gmail.com>
2024-08-16 20:42:17 +0600

    added database and collections

--- index.js
+++ index.js
@@ -26,7 +26,12 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
 
+    // Database decl
+    const shopDB = client.db("shopCommerce");
+    const productsCollection = shopDB.collection("products");
 
+
+    // my code goes here
 
 
     // Send a ping to confirm a successful connection
